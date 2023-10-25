a135347d16d95438f61d45871a86cd0fc374121e
Paul Bachmann <s4005036@ba-sachsen.de>
2023-10-25 21:32:09 +0200

    First phase of game map implementation

---
+++ Game_Board.java
@@ -0,0 +1,12 @@
+public class Game_Board {
+    Row row;
+
+    public Game_Board(){
+        this.row = new Row();
+        // Game_Board has rows
+    }
+
+    public void Initialize_Empty_Board(int width){
+
+    }
+}
--- Player.java
+++ Player.java
@@ -1,5 +1,6 @@
 public class Player{
-    public void Take_Turn(){
+    public void Take_Turn(Game_TUI game_TUI){
+        //Calling necessary methods for 1 player turn
         
     }
 }
