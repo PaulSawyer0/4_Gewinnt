25ebe154af7f87fbeeb2d68c1a8347b2db4b3d6d
Paul Bachmann <s4005036@ba-sachsen.de>
2023-10-28 19:20:46 +0200

    Starting work at comparison.

--- Comparison.java
+++ Comparison.java
@@ -1,8 +1,30 @@
 public class Comparison {
     
-    public void Compare_Stones(Game_TUI game_tui, int win_condition){
+    public void Compare_Stones(Game_TUI game_tui, int player_num, int win_condition){
         //Get head clas Game_TUI, needed number of matching stones to win
 
-        
+        int row_tracker = 0;
+        // Track which row is currently being processed
+
+        for (Row row : game_tui.game_board.map){
+            for (int target_pos : row.coloumns){
+
+            }
+
+        }
+    }
+
+    public void Compare_Down(Game_TUI game_tui, int player_num, int win_condition){
+
+        // ***********
+        // *Parameter*
+        // ***********
+
+        // game_tui        = head class
+        // player_num      = Number that needs to match
+        // win_condition   = How many matches necessary for win
+
+        //Compare below current stone        
+
     }
 }
