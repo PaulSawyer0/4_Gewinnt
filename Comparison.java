6481118893b432b8d376d0e0b5d6a3d607322d6f
Paul Bachmann <s4005036@ba-sachsen.de>
2023-10-29 00:01:06 +0200

    Other comparison methods still running faulty for some reason

--- Comparison.java
+++ Comparison.java
@@ -58,36 +58,117 @@ public class Comparison {
     // ************
 
     // Using more than 2 parameters makes me throw up, still only viable way as for now
-    // Following methods do nothing besides conditional testing
+    // Following methods do nothing besides initialising, conditional testing
 
     // game_tui            = head class
     // player_num          = current player number
     // win_condition       = number of matches needed for win
     // row                 = current row
     // coloumn             = current coloumn
+    // index                  = Current comparison index
+    // match_tracker          Keep track of previous matches
     
     public void Comp_Init(Game_TUI game_tui, int player_num, int win_condition, int row, int coloumn){
         int match_tracker = 0;
-
         try{
             for (int i = 0; i < win_condition; ++i){
-                if (game_tui.game_board.map.get(row + i).coloumns.get(coloumn) == player_num){
-                    // Iterate 'down' rows
+                if (Comp_Down(game_tui, player_num, win_condition, row, coloumn, i)     == true ||
+                Comp_Left(game_tui, player_num, win_condition, row, coloumn, i)         == true ||
+                Comp_Right(game_tui, player_num, win_condition, row, coloumn, i)        == true ||
+                Comp_Lower_Left(game_tui, player_num, win_condition, row, coloumn, i)   == true ||
+                Comp_Lower_Right(game_tui, player_num, win_condition, row, coloumn, i)  == true ||
+                Comp_Upper_Left(game_tui, player_num, win_condition, row, coloumn, i)   == true ||
+                Comp_Upper_Right(game_tui, player_num, win_condition, row, coloumn, i)  == true ){
+                    // check if one of the 7 comparisons was successful
+                    // increment tracker if true
                     ++match_tracker;
                 }
-
-                if (match_tracker == win_condition){
-                    // Call Winner_Notificatio method
-                    // ==> end of game
-                    System.out.println("\nSUCCESS");
-                }
             }
+            
+            Match_Tracker_Test(match_tracker, win_condition);
+            //  if successful match was achieved
         }
 
         catch(java.lang.IndexOutOfBoundsException OutOfBounds){}
     }
 
-    public void Comp_Down(Game_TUI game_tui, int player_num, int win_condition, int row, int coloumn){
+    public boolean Comp_Down(Game_TUI game_tui, int player_num, int win_condition, int row, int coloumn, int i){
+        boolean match = false;
+                if (game_tui.game_board.map.get(row + i).coloumns.get(coloumn) == player_num){
+                    // Iterate 'down' rows
+                    match = true;
+                }
 
+        // if we do have a match, return match = true
+        return match;
+    }
+    
+    public boolean Comp_Left(Game_TUI game_tui, int player_num, int win_condition, int row, int coloumn, int i){
+        boolean match = false;
+                if (game_tui.game_board.map.get(row).coloumns.get(coloumn - i) == player_num){
+                    // Iterate to left index of same row
+                    match = true;
+                }
+
+        // if we do have a match, return match = true
+        return match;
+    }
+    public boolean Comp_Right(Game_TUI game_tui, int player_num, int win_condition, int row, int coloumn, int i){
+        boolean match = false;
+                if (game_tui.game_board.map.get(row).coloumns.get(coloumn + i) == player_num){
+                    // Iterate to right index of same row
+                    match = true;
+                }
+
+        // if we do have a match, return match = true
+        return match;
+    }
+    public boolean Comp_Lower_Left(Game_TUI game_tui, int player_num, int win_condition, int row, int coloumn, int i){
+        boolean match = false;
+                if (game_tui.game_board.map.get(row + i).coloumns.get(coloumn - i) == player_num){
+                    // Iterate down rows to left
+                    match = true;
+                }
+
+        // if we do have a match, return match = true
+        return match;
+    }
+    public boolean Comp_Lower_Right(Game_TUI game_tui, int player_num, int win_condition, int row, int coloumn, int i){
+        boolean match = false;
+                if (game_tui.game_board.map.get(row + i).coloumns.get(coloumn + i) == player_num){
+                    // Iterate down rows to right
+                    match = true;
+                }
+
+        // if we do have a match, return match = true
+        return match;
+    }
+    public boolean Comp_Upper_Left(Game_TUI game_tui, int player_num, int win_condition, int row, int coloumn, int i){
+        boolean match = false;
+                if (game_tui.game_board.map.get(row - i).coloumns.get(coloumn - i) == player_num){
+                    // Iterate up rows to left
+                    match = true;
+                }
+
+        // if we do have a match, return match = true
+        return match;
+    }
+    
+    public boolean Comp_Upper_Right(Game_TUI game_tui, int player_num, int win_condition, int row, int coloumn, int i){
+        boolean match = false;
+                if (game_tui.game_board.map.get(row - i).coloumns.get(coloumn + i) == player_num){
+                    // Iterate up rows to right
+                    match = true;
+                }
+
+        // if we do have a match, return match = true
+        return match;
+    }
+    public void Match_Tracker_Test(int match_tracker, int win_condition){
+                if (match_tracker == win_condition){
+                    // Call Winner_Notificatin method
+                    // ==> end of game
+                    System.out.println("\nSUCCESS");
+                }
     }
 }
--- Program.java
+++ Program.java
@@ -4,11 +4,11 @@ public class Program {
         Game_TUI game_TUI = new Game_TUI();
         game_TUI.game_board.Initialize_Empty_Board(6, 6);
 
-        int target = game_TUI.game_board.input.Choose_Coloumn(game_TUI, 3);
+        int target = game_TUI.game_board.input.Choose_Coloumn(game_TUI, 6);
         game_TUI.game_board.input.Update_Map(game_TUI, target, 2);
-        target = game_TUI.game_board.input.Choose_Coloumn(game_TUI, 3);
+        target = game_TUI.game_board.input.Choose_Coloumn(game_TUI,6);
         game_TUI.game_board.input.Update_Map(game_TUI, target, 2);
-        target = game_TUI.game_board.input.Choose_Coloumn(game_TUI, 3);
+        target = game_TUI.game_board.input.Choose_Coloumn(game_TUI, 6);
         game_TUI.game_board.input.Update_Map(game_TUI, target, 2);
         game_TUI.game_board.Print_Coloumn_Header(6);
         game_TUI.game_board.Print_Row_Coloumn(game_TUI);
