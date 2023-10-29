b3b71754107ca1a7aedd53c014030231227b0270
Paul Bachmann <s4005036@ba-sachsen.de>
2023-10-29 12:32:23 +0100

    Working comparisons for each direction implemented

--- Comparison.java
+++ Comparison.java
@@ -1,11 +1,5 @@
 public class Comparison {
     
-    public void Compare_Stones(Game_TUI game_tui, int player_num, int win_condition){
-        //Get head clas Game_TUI, needed number of matching stones to win
-        Comp_Down(game_tui, player_num, win_condition);
-        // Comp_Left(game_tui, player_num, win_condition);
-        Comp_Right(game_tui, player_num, win_condition);
-
         // ********************
         // *Rework in Progress*
         // ********************
@@ -21,6 +15,31 @@ public class Comparison {
         // of OOP code.
 
         // Tldr: Something went wrong in the conditional tests outside of method comparisons
+
+        // *******************
+        // *Static Parameters*
+        // *******************
+
+        // *Directions*
+
+        // 1   =   Compare down
+        // 2   =   Compare Left
+        // 3   =   Compare Right
+        // 4   =   Compare Lower Left
+        // 5   =   Compare Lower Right
+        // 6   =   Compare Upper Left
+        // 7   =   Compare Upper Right
+
+    public void Compare_Stones(Game_TUI game_tui, int player_num, int win_condition){
+        //Get head clas Game_TUI, needed number of matching stones to win
+        Comp_Down(game_tui, player_num, win_condition);
+        Comp_Left(game_tui, player_num, win_condition);
+        Comp_Right(game_tui, player_num, win_condition);
+        Comp_Lower_Left(game_tui, player_num, win_condition);
+        Comp_Lower_Right(game_tui, player_num, win_condition);
+        Comp_Upper_Left(game_tui, player_num, win_condition);
+        Comp_Upper_Right(game_tui, player_num, win_condition);
+
         
     }
 
@@ -38,8 +57,7 @@ public class Comparison {
                         int result = 0;
                         try{
                             switch (direction) {
-                                // Putting the comparison into another method would take 8 parameters to achieve
-                                // in current state
+                                // Putting the comparison into another method would take 8 parameters to work
                                 // Nuh uh
                                 case 1:
                                     // Compare down
@@ -106,28 +124,6 @@ public class Comparison {
         int direction = 1;
         Comp_Loop(game_tui, player_num, win_condition, direction);
     }
-        // for (int row = 0; row < game_tui.game_board.map.size(); ++row){
-        //     try{
-        //         for (int coloumn = 0; coloumn < game_tui.game_board.map.get(coloumn).coloumns.size(); ++coloumn){
-        //             int match_tracker = 0;
-        //             for (int i = 0; i < win_condition; ++i){
-        //                 try{
-        //                     int result =  game_tui.game_board.map.get(row + i).coloumns.get(coloumn);
-        //                     if (result == player_num){
-        //                         // Iterate 'down' rows
-        //                     System.out.println(result);
-        //                     ++match_tracker;
-        //                     Match_Tracker_Test(match_tracker, win_condition);
-        //                 }
-
-                        
-        //                 }
-        //                 catch (java.lang.IndexOutOfBoundsException OutOfBounds){}
-        //         }
-        //     }
-        // }
-        //     catch (java.lang.IndexOutOfBoundsException OutOfBounds){}
-        // }
 
     public void Comp_Left(Game_TUI game_tui, int player_num, int win_condition){
         int direction = 2;
@@ -138,29 +134,26 @@ public class Comparison {
         int direction = 3;
         Comp_Loop(game_tui, player_num, win_condition, direction);
     }
-        // for (int row = 0; row < game_tui.game_board.map.size(); ++row){
-        //     try{
-        //         for (int coloumn = 0; coloumn < game_tui.game_board.map.get(coloumn).coloumns.size(); ++coloumn){
-        //             int match_tracker = 0;
-        //             for (int i = 0; i < win_condition; ++i){
-        //                 try{
-        //                     int result =  game_tui.game_board.map.get(row).coloumns.get(coloumn - i);
-        //                     if (result == player_num){
-        //                         // Iterate 'down' rows
-        //                     System.out.println(result);
-        //                     ++match_tracker;
-        //                     Match_Tracker_Test(match_tracker, win_condition);
-        //                 }
+    
+    public void Comp_Lower_Left(Game_TUI game_tui, int player_num, int win_condition){
+        int direction = 4;
+        Comp_Loop(game_tui, player_num, win_condition, direction);
+    }
+    
+    public void Comp_Lower_Right(Game_TUI game_tui, int player_num, int win_condition){
+        int direction = 5;
+        Comp_Loop(game_tui, player_num, win_condition, direction);
+    }
 
-                        
-        //                 }
-        //                 catch (java.lang.IndexOutOfBoundsException OutOfBounds){}
-        //         }
-        //     }
-        // }
-        //     catch (java.lang.IndexOutOfBoundsException OutOfBounds){}
-        // }
-        // if we do have a match, return match = true
+    public void Comp_Upper_Left(Game_TUI game_tui, int player_num, int win_condition){
+        int direction = 6;
+        Comp_Loop(game_tui, player_num, win_condition, direction);
+    }
+    
+    public void Comp_Upper_Right(Game_TUI game_tui, int player_num, int win_condition){
+        int direction = 7;
+        Comp_Loop(game_tui, player_num, win_condition, direction);
+    }
 
     public void Match_Tracker_Test(int match_tracker, int win_condition){
                 if (match_tracker == win_condition){
