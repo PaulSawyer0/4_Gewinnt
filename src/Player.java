e826d3727d84cf5ba4cb4093341b00267dea2c18
Paul Bachmann <pa.bachmann@gmx.net>
2023-10-30 22:24:06 +0100

    Further yassification

--- src/Player.java
+++ src/Player.java
@@ -9,12 +9,34 @@ public class Player{
         game_TUI.game_board.Print_Coloumn_Header_Enclosure(game_TUI);
         game_TUI.game_board.Print_Row_Coloumn(game_TUI);
         game_TUI.game_board.Print_Coloumn_Header_Enclosure(game_TUI);
+
         Print_Current_Player(game_TUI, player_num);
+        Print_Input_Prompt(game_TUI);
 
         int coloumn = game_TUI.game_board.input.Choose_Coloumn(game_TUI);
         game_TUI.game_board.input.Update_Map(game_TUI, coloumn, player_num);
         game_TUI.game_board.comp.Compare_Stones(game_TUI, player_num);
         game_TUI.game_board.input.Check_Available_Space(game_TUI);
+    }
+
+    public void Print_Input_Prompt(Game_TUI game_tui){
+        System.out.println("\n");
+        try{
+            switch (game_tui.lang_int) {
+                case 1:
+                    System.out.println("Choose a coloumn: ");
+                    break;
+            
+                case 2:
+                    System.out.println("Wähle eine Spalte: ");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        catch(java.lang.ArithmeticException OutOfBounds){}
+        
     }
 
     public void Print_Current_Player(Game_TUI game_tui, int player_num){
